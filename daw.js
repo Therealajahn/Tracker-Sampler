01aa1c9fd9e47cafb4077b2406a47a6aad57a867
Therealajahn <therealajahn@gmail.com>
2020-11-04 23:13:47 -0600

    results of stream coding

---
+++ daw.js
@@ -0,0 +1,14 @@
+document.getElementById('prime-audio').addEventListener('click', async (e) => {
+    await Tone.start();
+    e.target.innerHTML = 'Audio Primed';
+})
+
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
+
+
+
+
